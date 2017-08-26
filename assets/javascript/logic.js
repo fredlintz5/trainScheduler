677b89699d1112968bace2900f4427477874ee1c
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2017-08-26 17:16:05 -0600

    added editable fields, added moment.js functionality

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -10,7 +10,7 @@ var config = {
   firebase.initializeApp(config);
 
 // Get a reference to the database service
-var database = firebase.database();
+var database = firebase.database().ref();
 
 // Create an instance of the GitHub provider object
 // var provider = new firebase.auth.GithubAuthProvider();
@@ -58,7 +58,7 @@ var database = firebase.database();
 // At the initial load and on subsequent data value changes, 
 // get a snapshot of the current data.
 // This callback keeps the page updated when a value changes in firebase.
-database.ref().on("value", function(snapshot) {
+database.on("value", function(snapshot) {
 	
 	if (snapshot.child('databaseArray').exists()) {
 
@@ -74,10 +74,11 @@ database.ref().on("value", function(snapshot) {
 
 // set global array variable
 var localArray = [];
+var editableIndexNumber;
 
 $('#submit').click(function(event) {
 
-	if ($('#name').val() === ""|| $('#dest').val() === ""|| $('#time').val() === ""|| $('#freq').val()=== "") {
+	if ($('#name').val() === ""|| $('#dest').val() === ""|| $('#time').val() === "") {
 		event.preventDefault();
 		alert("Please fill out all form fields");
 
@@ -86,18 +87,29 @@ $('#submit').click(function(event) {
 
 		var tName = $('#name').val();
 		var tDest = $('#dest').val();
-		var tFreq = $('#freq').val();
-		var tTime = moment($('#time').val(), 'h:mm').format('h:mm');
+		var tTime = moment($('#time').val(), 'HH:mm A').format('h:mm A');
 		console.log(tTime);
-		var now = moment().format('lt');
-		console.log(now);
-		var tRemain = "";
+		var tTimeUnix = moment($('#time').val(), 'HH:mm A').format('X');
+		var currentTime = moment().format('h:mm A');
+		console.log(currentTime);
+		var currentTimeUnix = moment().format('X');
+
+		
+
+		if (tTimeUnix > currentTimeUnix) {
+			console.log('if');
+			var tRemain = moment(tTime, 'HH:mm A').fromNow(true) + " from now";
+
+
+		} else {
+			console.log('else');
+			var tRemain = "This Train has already left";
+		}
 		
 		var trainObject = {
 		  	name: tName,
 		  	dest: tDest,
 		  	time: tTime,
-		  	freq: tFreq,
 		  	remain: tRemain
 		  };
 
@@ -106,9 +118,8 @@ $('#submit').click(function(event) {
 		$('#name').val('');
 		$('#dest').val('');
 		$('#time').val('');
-		$('#freq').val('');
 
-		database.ref().set({
+		database.set({
 		  databaseArray:localArray
 		})
 
@@ -130,13 +141,14 @@ $(".container").on("click", "#logout", function () {
 });
 
 
-$('tbody').on("click", "button", function() {
+//remove train data from page, and local/remote arrays
+$('tbody').on("click", "#remove", function() {
 
     var indexNumber = $(this).data('index');
 	
 	localArray.splice(indexNumber, 1);
 
-	database.ref().set({
+	database.set({
   		databaseArray:localArray
 	})
 
@@ -146,6 +158,100 @@ $('tbody').on("click", "button", function() {
 });
 
 
+//edit train data on page, and update remote data
+$('tbody').on("click", "#edit", function() {
+
+	console.log(localArray);
+    editableIndexNumber = $(this).data('index');
+	var offset = $(this).offset();
+
+	$('.container').css('opacity', '0.1');
+    $('#hiddenFields').removeClass('hide');
+    $('#hiddenFields').css({
+    		top: offset.top -700,
+    	});
+});
+
+
+// submit edited inputs to replace corresponding values
+$('#editedSubmit').click(function() {
+
+	if ($('#editedName').val() === ""|| $('#editedDest').val() === ""|| $('#editedTime').val() === "") {
+		alert("Please fill out all form fields");
+
+	} else {
+
+	    var editedName = $('#editedName').val();
+		var editedDest = $('#editedDest').val();
+		var editedTime = moment($('#editedTime').val(), 'HH:mm A').format('h:mm A');
+		console.log('click');
+		var tTimeUnix = moment($('#time').val(), 'HH:mm A').format('X');
+		var currentTime = moment().format('h:mm A');
+		var currentTimeUnix = moment().format('X');
+		
+		if (tTimeUnix > currentTimeUnix) {
+			console.log('if');
+			var editedRemain = moment(editedTime, 'HH:mm A').fromNow(true) + " from now";
+
+		} else {
+			console.log('else');
+			var editedRemain = "This Train has already left";
+		}
+
+
+		localArray.splice(editableIndexNumber, 1, {
+			name: editedName,
+		  	dest: editedDest,
+		  	time: editedTime,
+		  	remain: editedRemain
+		});
+
+
+		var editedName = $('#editedName').val("");
+		var editedDest = $('#editedDest').val("");
+		var editedTime = $('#editedTime').val("");
+
+		database.set({
+			databaseArray:localArray
+		})
+
+	 	$('tbody').empty();
+
+	 	$('.container').css('opacity', '1.0');
+    	$('#hiddenFields').addClass('hide');
+
+
+		loadTrains();
+	}
+});
+	
+
+	
+
+	// if (tTimeUnix > currentTimeUnix) {
+	// 	console.log('if');
+	// 	// var tRemain = moment(tTime, 'HH:mm A').diff(moment(), "minutes");
+	// 	var tRemain = moment(tTime, 'HH:mm A').fromNow(true) + " from now";
+
+
+	// } else {
+	// 	console.log('else');
+	// 	// var tRemain = moment().diff(moment(tTime, 'HH:mm A'), "minutes");
+	// 	// var tRemain = moment(tTime, 'HH:mm A').toNow(true) + " to now";
+	// 	var tRemain = "This Train has already left";
+	// }
+
+
+	// database.set({
+ //  		databaseArray:localArray
+	// })
+
+ // 	$('tbody').empty();
+	// loadTrains();	
+
+
+
+//load train data form localArray to page
 function loadTrains() {
 
 	for (var i = 0; i < localArray.length; i++) {
@@ -156,15 +262,15 @@ function loadTrains() {
 		newData.append("<td>"+localArray[i].name);
 		newData.append("<td>"+localArray[i].dest);
 		newData.append("<td>"+localArray[i].time);
-		newData.append("<td>"+localArray[i].freq);
+		// newData.append("<td>"+localArray[i].freq);
 		newData.append("<td>"+localArray[i].remain);
-		newData.append("<button data-index='" + [i] + "'class='btn btn-danger'>X");
+		newData.append("<button data-index='" + [i] + "'class='btn btn-success glyphicon' id='edit'>&#x270f;");
+		newData.append("<button data-index='" + [i] + "'class='btn btn-danger glyphicon' id='remove'>&#xe014;");
 
 		$('tbody').append(newData);
 	}
 
 	var homePage = "https://fredlintz5.github.io/trainScheduler/";
-	console.log(homePage);
 
 	if (window.location.href == homePage) {
 		$('tr > button').toggleClass('hide');
